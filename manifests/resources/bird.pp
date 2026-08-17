f82f2308c9f7b805f54c3dafb24ecd8baa63f5aa
Florian Steinel <7137+fsteinel@users.noreply.github.com>
2026-08-17 17:04:11 +0200

    Add bird, bgp routing daemon, version 3

--- manifests/resources/bird.pp
+++ manifests/resources/bird.pp
@@ -1,4 +1,36 @@
 class ffnord::resources::bird (
+  #https://pkg.labs.nic.cz/doc/?project=bird
+  apt::key {
+    'cznic-labs-pkg.gpg':
+      key => '0xAB6A303124019B64',
+      key_source => 'https://pkg.labs.nic.cz/gpg';
+  }
+
+  apt::source {
+    'bird':
+      location   => 'https://pkg.labs.nic.cz/bird3',
+      release    => $::lsbdistcodename,
+      repos      => 'main';
+  }
+
+  package {
+    'bird3':
+      ensure => installed,
+      notify => Service['bird3'],
+      require => [
+        Apt::Source['bird']
+      ];
+  }
+
+  service {
+    'bird':
+      ensure => running,
+      enable => true,
+      require => [
+        Package['bird3'],
+      ];
+  }
+
   $include_bird4 = $ffnord::params::include_bird4,
   $include_bird6 = $ffnord::params::include_bird6,
 ) inherits ffnord::params {
@@ -6,15 +38,6 @@ class ffnord::resources::bird (
     '/etc/bird/':
       ensure => directory,
       mode => '0755';
-  }
-  if($::lsbdistcodename=='wheezy'){
-    file { '/etc/apt/preferences.d/bird':
-      ensure => file,
-      mode => '0644',
-      owner => root,
-      group => root,
-      source => 'puppet:///modules/ffnord/etc/apt/preferences.d/bird';
-    }
   }
 
   ffnord::firewall::service { 'bird':
